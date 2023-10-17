53172439802117a7c1c6ebcada311eb2397da4a8
Renat <remch2013ya.ru>
2023-10-17 19:56:35 +0400

    решение задач

---
+++ Задача1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число N: ");
+        int N = int.Parse(Console.ReadLine());
+        PrintNaturalNumbers(N);
+    }
+
+    static void PrintNaturalNumbers(int N)
+    {
+        if (N >= 1)
+        {
+            Console.Write(N);
+            Console.Write(", ");
+            PrintNaturalNumbers(N - 1);
+        }
+    }
+}
---
+++ Задача2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int M = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        if (M > N)
+        {
+            Console.WriteLine("M должно быть меньше или равно N.");
+        }
+        else
+        {
+            int sum = CalculateSumOfNaturalNumbers(M, N);
+            Console.WriteLine($"Сумма натуральных чисел от {M} до {N} равна: {sum}");
+        }
+    }
+
+    static int CalculateSumOfNaturalNumbers(int start, int end)
+    {
+        int sum = 0;
+        for (int i = start; i <= end; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+}
+
+
